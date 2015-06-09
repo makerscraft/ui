318d8a249f69905a8f86ef719104d9752002c279
Kara Leary <learykara@gmail.com>
2015-06-09 10:30:48 -0400

    use classnames passed into component

--- src/SidebarLayout.jsx
+++ src/SidebarLayout.jsx
@@ -1,16 +1,20 @@
 const React = require('react');
+const classnames = require('classnames');
 
 require('./styles/sidebarlayout.less');
 
 const SidebarLayout = React.createClass({
   render() {
     const {sidebarContent, mainContent, sidebarClasses, mainClasses} = this.props;
+    const sidebarClassNames = classnames('sidebar', sidebarClasses);
+    const mainClassNames = classnames('main', mainClasses);
+
     return (
       <div className="sidebar-layout-container">
-        <div className="sidebar">
+        <div className={sidebarClassNames}>
           {sidebarContent}
         </div>
-        <div className="main">
+        <div className={mainClassNames}>
           {mainContent}
         </div>
       </div>
