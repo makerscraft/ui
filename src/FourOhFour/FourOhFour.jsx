1db6190ff57a4720f0337a2832253918c9d5ea48
Olex Ponomarenko <tholex@gmail.com>
2015-07-10 12:15:09 -0400

    Update FourOhFour page.

--- src/FourOhFour/FourOhFour.jsx
+++ src/FourOhFour/FourOhFour.jsx
@@ -12,7 +12,8 @@ class FourOhFour extends React.Component {
         return (
             <div className="tfui-error-page">
                 <h1>404</h1>
-                <h3>Page not found</h3>
+                <p>The page you are looking for couldn't be found.<br/> You can try 
+                <a href="javscript:history.go(-1);">going back</a>.</p>
             </div>
         )
     }
